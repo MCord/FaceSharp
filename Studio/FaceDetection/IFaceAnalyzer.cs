2a5a89f8012798c8f147543657f26ffc2d5a2ada
Mehran Kordi <mehran.kordi@rfq-hub.com>
2014-10-21 00:06:10 +0200

    added face detection abstractions.

---
+++ Studio/FaceDetection/IFaceAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿namespace Studio
+{
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Media;
+
+    interface IFaceAnalyzer
+    {
+        IEnumerable<IFacialFeature> ExtractFacialFeatures(ImageSource image);
+    }
+
+    interface IFacialFeature
+    {
+        int Id { get; }
+        string Name { get; }
+        Point Location { get; }
+    }
+}
